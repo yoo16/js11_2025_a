40ebe6d0d31ee829e7e58b2b8a29dd717ba646d0
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2025-11-05 10:23:58 +0900

    update

--- 04_function/js/functions.js
+++ 04_function/js/functions.js
@@ -26,9 +26,9 @@ document.getElementById("math_ceil").textContent = Math.ceil(height)
 // TODO: 切り捨て: floor()
 document.getElementById("math_floor").textContent = Math.floor(height)
 // TODO: 最大値: max()
-document.getElementById("math_max").textContent
+document.getElementById("math_max").textContent = Math.max(1, 5, 10, 3)
 // TODO: 最小値: min()
-document.getElementById("math_min").textContent
+document.getElementById("math_min").textContent = Math.min(1, 5, 10, 3)
 
 let current = 0
 // TODO: 1秒毎にカウント: setInterval()
@@ -38,6 +38,10 @@ document.getElementById("count").textContent = current
 // ユーザ定義関数
 // ==============================
 // TODO: calculate() を定義: 引数 x の2乗を返す
+function calculate(x) {
+    let result = x ** 2
+    return result
+}
 
 // TODO: calculate() に 8 を渡して結果を表示
 document.getElementById("calc1").textContent
